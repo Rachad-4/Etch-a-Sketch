04f7b27af31ff14f24913ed8291318183350c350
Rachad-4 <rachadneyland@gmail.com>
2024-01-30 15:56:08 -0600

    Add ramdom color functionality squashed existing bugs.
    Users can now use random colors when drawing.

--- app.js
+++ app.js
@@ -1,15 +1,48 @@
 const grid = document.querySelector(`.grid`);
 const resizeBTN = document.querySelector(`#resize`);
 const eraseBTN = document.querySelector(`#erase`);
-const rainbowBTN = document.querySelector(`#rainbow`);
+const randomBTN = document.querySelector(`#rainbow`);
 const resetBTN = document.querySelector(`#reset`);
 
 let numOfBoxes = 75; 
 let divs; //defines "divs" variable that will hold list of divs
 let clear = false;
+let randomColor = false;
 
 createGraph();
 draw();
+
+randomBTN.addEventListener(`click`, ()=>{
+    randomColor = !randomColor;
+    clear = false;
+
+    eraseBTN.classList.remove(`selected`);
+    randomBTN.classList.toggle('selected');
+
+    if (randomColor == true && clear == false) {
+        randomizeColor();
+    } else if (clear == false && randomColor == false) {
+        draw();
+    }else if (clear == true && randomColor == false) {
+        erase();
+    }    
+})
+
+eraseBTN.addEventListener(`click`, ()=>{
+    clear = !clear; 
+    randomColor = false;
+
+    randomBTN.classList.remove(`selected`);
+    eraseBTN.classList.toggle("selected");
+
+    if (clear == true && randomColor == false) {
+        erase();
+    } else if (clear == false && randomColor == false) {
+        draw();
+    } else if (randomColor == true && clear == false) {
+        randomizeColor();
+    }
+})
 
 resetBTN.addEventListener(`click`, (button)=>{
     animateButton(button);
@@ -62,11 +95,18 @@ function erase(){
     for (let i = 0; i < divs.length; i++) {
         divs[i].onmouseenter = () => {
             divs[i].classList.remove("black");
+            divs[i].style.backgroundColor = null;
         }
     }
 }
 
 function reset(){
+    clear = false;
+    randomColor = false;
+
+    eraseBTN.classList.remove("selected");
+    randomBTN.classList.remove("selected");
+
     divs = document.querySelectorAll("div");
 
     for(let i = 0; i < numOfBoxes; i++){
@@ -75,9 +115,23 @@ function reset(){
     }
 
     createGraph();
+    draw();
 } 
 
 function animateButton(button){
     button.target.classList.add(`selected`);
     setTimeout(function(){button.target.classList.remove(`selected`)},120);
+}
+
+function randomizeColor(){
+    
+    for (let i = 0; i < divs.length; i++) {
+        let randomColor1 = Math.ceil(Math.random()*255); 
+        let randomColor2 = Math.ceil(Math.random()*255); 
+        let randomColor3 = Math.ceil(Math.random()*255); 
+
+        divs[i].onmouseenter = () => {
+            divs[i].setAttribute(`style`, `background-color: rgb(${randomColor1},${randomColor2},${randomColor3});`);
+        }
+    }
 }
